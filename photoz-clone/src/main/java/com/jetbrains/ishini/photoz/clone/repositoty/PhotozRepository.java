2fb0d874819d9435e43cef5b08a05897b5586e5a
ishii1218 <uppaleei@gmail.com>
2024-05-28 14:56:17 +0530

    firstCommit

---
+++ photoz-clone/src/main/java/com/jetbrains/ishini/photoz/clone/repositoty/PhotozRepository.java
@@ -0,0 +1,8 @@
+package com.jetbrains.ishini.photoz.clone.repositoty;
+
+import com.jetbrains.ishini.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+
+}
---
+++ photoz-clone/src/test/java/com/jetbrains/ishini/photoz/clone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.ishini.photoz.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
